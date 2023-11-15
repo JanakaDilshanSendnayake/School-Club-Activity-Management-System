76b3fad69ae781a78770f56d28827794d7af7c5c
Abdul-Shahid-2002 <shahidafham7773@gmail.com>
2023-11-15 13:53:37 +0530

    new Events controller class and made edits to events fxml file

---
+++ OOD-Group_CW/src/main/java/SceneControlersClubAdvisors/CAEvents.java
@@ -0,0 +1,27 @@
+package SceneControlersClubAdvisors;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.layout.AnchorPane;
+
+public class CAEvents {
+
+    @FXML
+    private ComboBox<?> ClubSelectionChoiceBox;
+
+    @FXML
+    private ComboBox<?> EventSelectionChoiceBox;
+
+    @FXML
+    private AnchorPane namesAnchorpane;
+
+    @FXML
+    private AnchorPane sidebar;
+
+    @FXML
+    void handleSave(ActionEvent event) {
+
+    }
+
+}
