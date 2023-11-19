9e5e7c49e21dd6d7ad6dfda670f726f8409b40e2
Abdul-Shahid-2002 <shahidafham7773@gmail.com>
2023-11-19 10:53:19 +0530

    connector class and updated module .info(added java.sql module)

--- OOD-Group_CW/src/main/java/module-info.java
+++ OOD-Group_CW/src/main/java/module-info.java
@@ -2,6 +2,7 @@ module OOD.Group.CW {
     requires  javafx.graphics;
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
     //requires charm.glisten;
 
     opens main;
